ac14650bc00c0c57d30bbabe13a4113f23fa66f9
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-02-02 21:18:48 -0800

    wip

--- Atom.c
+++ Atom.c
@@ -15,6 +15,36 @@ void Atom_system_init() {
 
 void Atom_system_done() {
   printf("--- Atom_system_done() ---\n");
+  int i;
+  for(i = 0; i < ATOM_BUCKET_SIZE; i++) {
+    AtomVector_del(av_buckets[i]);
+  }
+}
+
+AtomVector* AtomVector_new() {
+  AtomVector* self = (AtomVector*) malloc(sizeof(AtomVector));
+  self->size = ATOMVECTOR_ISIZE;
+  self->lidx = 0;
+  self->list = (Atom**) malloc(self->size * sizeof(Atom));
+  return self;
+}
+
+void AtomVector_push(AtomVector self, Atom* atom) {
+  self->list[self->lidx++] = atom;
+  if(self->lidx == self->size) {
+    self->list = (Atom**) realloc(self->list, 2 * self->lidx);
+    self->lidx = 2 * self->lidx;
+  }
+}
+
+void AtomVector_del(AtomVector* self) {
+  int i;
+  size_t list_size = self->size;
+  for(i = 0; i < size; i++) {
+    Atom_del(self->list[i]);
+  }
+  free(self->list);
+  free(self);
 }
 
 size_t Atom_key(char* str) {
@@ -29,10 +59,25 @@ size_t Atom_key(char* str) {
 
 Atom* Atom_find(char* str) {
   size_t key = Atom_key(str);
+  return av_buckets[key];
 }
 
 Atom* Atom_new(char* str) {
+  Atom* self = Atom_find(str);
+  if(self != NULL) {
+    return self;
+  }
   Atom* self = malloc(sizeof(Atom));
+  size_t str_len = strlen(str);
+  self->str_len = str_len;
+  size_t buflen = str_len+1;
+  self->str = malloc(buflen);
+  strcopy(self->str, str);
   return self;
+}
+
+void Atom_del(Atom* self) {
+  free(self->str);
+  free(self);
 }
 
--- Atom.h
+++ Atom.h
@@ -4,6 +4,9 @@
 #define ATOM_BUCKET_SIZE 2048;
 #define ATOMVECTOR_ISIZE = 1;
 
+void Atom_system_init();
+void Atom_system_done();
+
 typedef struct Atom Atom;
 struct Atom {
   size_t str_len;
@@ -13,15 +16,14 @@ struct Atom {
 typedef struct AtomVector AtomVector;
 struct AtomVector {
   size_t size;
-  size_t tail;
-  Atom* head;
+  size_t lidx;
+  Atom** list;
 }
 
 AtomVector* AtomVector_new();
-void AtomVector_del(AtomVector* del);
+void AtomVector_push(AtomVector* self, Atom* atom);
+void AtomVector_del(AtomVector* self);
 
-void Atom_system_init();
-void Atom_system_done();
 size_t Atom_key(char* str);
 Atom* Atom_find(char* str);
 Atom* Atom_new(char* str);
