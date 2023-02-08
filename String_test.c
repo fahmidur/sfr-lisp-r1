7d31f2535ea224ca081f2b49fed763a4cbe1b72a
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-02-08 14:34:58 -0800

    wip

--- String_test.c
+++ String_test.c
@@ -13,6 +13,9 @@ int main(int argc, char** argv) {
   nassert(strcmp("Hello", str1->buf) == 0);
   nassert(str1->len == 5);
   nassert(String_len(str1) == 5);
+  String_zero(str1);
+  nassert(str1->len == 0);
+  nassert(String_len(str1) == 0);
 
   String* str2 = String_new("Hello\n\n");
   String_chomp(str2);
