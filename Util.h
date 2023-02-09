b1149a981220474d169d9b6834ed1d58d9628dd1
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-02-09 15:14:24 -0800

    wip

--- Util.h
+++ Util.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Not guaranteed to be unique for all strings.
+// This is not a cryptographically strong hash.
 size_t Util_cstr_hash_djb2(char* str);
+
 size_t Util_sizet_max(size_t a, size_t b);
 
 #endif
