b78421864da0f1ff809ea187f51a748753915e3e
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-02-09 17:57:22 -0800

    wip

--- Object.c
+++ Object.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "Symbol.h"
+#include "String.h"
+#include "Number.h"
 #include "Object.h"
 
 static ObjectSystem object_system;
+
+static Symbol* SYMBOL_SYMBOL;
+static Symbol* SYMBOL_STRING;
+static Symbol* SYMBOL_NUMBER;
 
 void Object_system_init() {
   printf("--- Object_system_init() ---\n");
   object_system.head = NULL;
   object_system.tail = NULL;
+  object_system.size = 0;
+
+  // TODO register the base types
+
+  SYMBOL_SYMBOL = Symbol_new("Symbol");
+  SYMBOL_STRING = Symbol_new("String");
+  SYMBOL_NUMBER = Symbol_new("Number");
+
+}
+
+Object* Object_new(Symbol* type, void* impl) {
+  Object* self = malloc(sizeof(Object));
+  // TODO guard against unknown types
+  self->type = type;
+  self->impl;
+  return self;
 }
 
 void Object_system_done() {
--- Object.h
+++ Object.h
@@ -5,19 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "Symbol.h"
+#include "String.h"
+#include "Number.h"
+
 typedef struct Object Object;
 struct Object {
-  Symbol* klass;
+  Symbol* type;
   Object* next;
   Object* prev;
-  void* impl;
+  void*   impl;
 };
 
 typedef struct ObjectSystem ObjectSystem;
 struct ObjectSystem {
   Object* head;
   Object* tail;
+  size_t  size;
 };
+
+Object* Object_new(Symbol* type, void* impl);
 
 void Object_system_init();
 void Object_system_done();
