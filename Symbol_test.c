6c01a796ee4cdf685085c4509c939a787dad1712
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-02-13 18:31:07 -0800

    wip

--- Symbol_test.c
+++ Symbol_test.c
@@ -12,13 +12,18 @@ int main(int argc, char** argv) {
   Symbol* apple1 = Symbol_new("apple");
   Symbol* apple2 = Symbol_new("apple");
   nassert(apple1 == apple2);
+  nassert(Symbol_cmp(apple1, apple2) == 0);
 
   Symbol* orange = Symbol_new("orange");
   nassert(apple1 != orange);
+  nassert(apple2 != orange);
+  nassert(Symbol_cmp(apple1, orange) != 0);
+  nassert(Symbol_cmp(apple2, orange) != 0);
 
   Symbol* dolly1 = Symbol_new("dolly");
   Symbol* dolly2 = Symbol_clone(dolly1);
   nassert(dolly1 == dolly2);
+  nassert(Symbol_cmp(dolly1, dolly2) == 0);
 
   Symbol_system_print();
   Symbol_system_done();
