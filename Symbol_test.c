acc24661a19a313219c81f3914b9e2be41003699
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-02-13 18:16:06 -0800

    wip

--- Symbol_test.c
+++ Symbol_test.c
@@ -16,12 +16,12 @@ int main(int argc, char** argv) {
   Symbol* orange = Symbol_new("orange");
   nassert(apple1 != orange);
 
-  Symbol_system_print();
-  Symbol_system_done();
-
   Symbol* dolly1 = Symbol_new("dolly");
   Symbol* dolly2 = Symbol_clone(dolly1);
   nassert(dolly1 == dolly2);
+
+  Symbol_system_print();
+  Symbol_system_done();
 
   nassert_report();
   return nassert_exit_code();
