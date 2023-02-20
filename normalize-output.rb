bb743aded7dda3a3f28dc7c208faa866e82df227
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-02-19 16:00:32 -0800

    wip

--- normalize-output.rb
+++ normalize-output.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+# The purpose of this is to normalize the output
+# between Racket and sfr-lisp.rb so that we can use this 
+# to test behavior against Racket.
+
 while(line=gets)
   line.gsub!(/#<LispProcedure.*\>/, '#<LispProcedure>')
   line.gsub!(/#<procedure.*\>/, '#<LispProcedure>')
