c864ea49c7911ad14546a140df4f4b5299fcef18
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-08-06 17:51:31 -0700

    function wip

--- Function.h
+++ Function.h
@@ -1,6 +1,11 @@
 #ifndef _SFR_FUNCTION
 #define _SFR_FUNCTION
 
+/**
+ * A wrapper around C Functions. 
+ * (Object.List) --> (FUNC) --> (Object)
+ **/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "Symbol.h"
@@ -10,13 +15,24 @@
 
 typedef struct Function Function;
 struct Function {
-  int arity;
-  Symbol* ret_type;
-  // args must be of type List or Hash
+  int arity;       // optional.
   Object* (impl*)(Object* args);
 };
 
-Function* Function_new(int arity, Symbol* ret_type) {
+Function* Function_new(Object* (impl*)(Object* args)) {
+  Function* self = calloc(1, sizeof(Function));
+  return self;
+}
+
+Object* Function_call(Function* self, Object* args) {
+  if(Object_type(args) != SYMBOL_LIST) {
+    return Object_return(Object_new(SYMBOL_ERROR, 0, Error_new("Function expecting args of type SYMBOL_LIST")))
+  }
+  if(self->arity != -1 && self->arity != Object_len(args)) {
+    return Object_return(Object_new(SYMBOL_ERROR, 0, Error_new("Function args length arity mismatch")));
+  }
+  // TODO
+  return NULL;
 }
 
 #endif
