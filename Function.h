61dbf646ce0c1740de016d24757169feae6f0a5e
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-08-20 21:18:52 -0700

    Function. wip

--- Function.h
+++ Function.h
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "Symbol.h"
 #include "Object.h"
 
@@ -21,9 +22,20 @@ struct Function {
 
 Function* Function_new(Object* (impl*)(Object* args), int arity);
 
-Object* Function_call_n(Function* self, Object* args);
+// Uses varargs from stdarg. 
+// Every argument is expected to be Object*
+// The argument length is taken from self->arity
+Object* Function_call_n(Function* self, ...);
+
+// Common for unary operator functions.
 Object* Function_call_1(Function* self, Object* arg1);
+
+// Common for binary operator functions.
 Object* Function_call_2(Function* self, Object* arg1, Object* arg2);
+
+// Call function for an Object.List args
+// Downside: Requires allocation of an Object.List for the args
+Object* Function_apply_n(Function* self, Object* args);
 
 #endif
 
