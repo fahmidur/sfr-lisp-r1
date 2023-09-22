5048fcf95fafbcc08fea6e4a61d689fb788d9743
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-09-21 20:21:58 -0700

    wip

--- leaky.c
+++ leaky.c
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
   printf("--- { leaky { ---\n");
   size_t nbytes = 16;
 
-  printf("malloc(%d)\n", nbytes);
+  printf("malloc(%ld)\n", nbytes);
   char* buf = malloc(nbytes);
 
   // We are intentionally not calling free on the bytes
