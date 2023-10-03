6daab067fb7d590149020bc5936bebacb3a64326
Syed Fahmidur Reza <s.f.reza@gmail.com>
2023-10-02 23:22:08 -0700

    wip

--- Lisp_test.c
+++ Lisp_test.c
@@ -18,8 +18,11 @@ int main(int argc, char** argv) {
 
   printf("\n=== === tc2 === ===\n");
   Object* tc2_string = QSTRING_NEW1("92.3");
-  Object* tc2_tokens = Object_accept(Lisp_tokenize(tc2_string));
-  ObjectUtil_eprintf("tc2_tokens = %v\n", tc2_tokens);
+  Object* tc2_tokens_got = Object_accept(Lisp_tokenize(tc2_string));
+  Object* tc2_tokens_exp = Object_new_list(1, 1, QNUMBER(92.3));
+  ObjectUtil_eprintf("tc2_tokens_got = %v\n", tc2_tokens_got);
+  ObjectUtil_eprintf("tc2_tokens_exp = %v\n", tc2_tokens_exp);
+  nassert(Object_cmp(tc2_tokens_got, tc2_tokens_exp) == 0);
 
   printf("\n=== === tc3 === ===\n");
   Object* tc3_string = QSTRING_NEW1("(- 123 23)");
