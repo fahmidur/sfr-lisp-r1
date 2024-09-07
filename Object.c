459c17b83aa8c101a6acc2fde5d41ea091d607c0
Syed Fahmidur Reza <s.f.reza@gmail.com>
2024-09-06 17:34:06 -0700

    donuts cleanup

--- Object.c
+++ Object.c
@@ -598,8 +598,8 @@ void Object_system_gc() {
   while(iter != NULL) {
     next = iter->next;
     if(iter->unreachable) {
-      ObjectUtil_eprintf("donuts. unreachable obj(p=%p rc=%d)\n", iter, iter->rc);
-      /* ObjectUtil_eprintf("donuts. unreachable obj(p=%p, rc=%d) = %v\n", iter, iter->rc, iter); */
+      /* ObjectUtil_eprintf("unreachable obj(p=%p rc=%d)\n", iter, iter->rc); */
+      /* ObjectUtil_eprintf("unreachable obj(p=%p, rc=%d) = %v\n", iter, iter->rc, iter); */
       iter->rc = 0;
     }
     iter = Object_gc(iter);
