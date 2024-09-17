04611dba374e5e7f788aff4db4915943ddaa0cab
Syed Fahmidur Reza <s.f.reza@gmail.com>
2024-09-16 18:36:23 -0700

    wip

--- Lisp.c
+++ Lisp.c
@@ -486,22 +486,36 @@ Object* Lisp_eval_sexp2(Object* sexp, Object* env) {
     // it is in this case that we can have various lisp forms
     op = Object_accept(Object_uop_head(sexp));
     opval = Lisp_eval_sexp2(op, env);
-    if(Object_type(opval) == SYMBOL_FUNCTION) {
-      // map each arg to values
-      opargs1 = Object_accept(Object_uop_rest(sexp));
-      opargs2 = Object_new_list(1, 0);
-      ListIter* iter = ListIter_new(opargs1->impl);
-      ListIter_head(iter);
-      while(!ListIter_at_end(iter)) {
-        tmp = Object_accept(ListIter_get_val(iter));
-        tmp2 = Object_accept(Lisp_eval_sexp2(tmp, env));
-        Object_bop_push(opargs2, tmp2);
-        Object_assign(&tmp, NULL);
-        Object_assign(&tmp2, NULL);
-        ListIter_next(iter);
+    if(Object_type(op) == SYMBOL_SYMBOL) {
+      if(op == QSYMBOL("define")) {
+        opargs1 = Object_accept(Object_uop_rest(sexp));
+        Object_top_hset(env, op, Lisp_eval_sexp2(opargs1, env));
       }
-      ListIter_del(iter);
-      ret = Object_bop_call(opval, opargs2);
+      if(op == QSYMBOL("set!")) {
+        // set the value of a variable that has been already defined
+        /* opargs1 = Object_accept(Object_uop_rest(sexp)); */
+        if(!Object_is_null(opval)) {
+          Object_reject(Object_top_hset(env, op, Lisp_eval_sexp2(opargs1, env)));
+        }
+      }
+      else
+      if(Object_type(opval) == SYMBOL_FUNCTION) {
+        // map each arg to values
+        opargs1 = Object_accept(Object_uop_rest(sexp));
+        opargs2 = Object_new_list(1, 0);
+        ListIter* iter = ListIter_new(opargs1->impl);
+        ListIter_head(iter);
+        while(!ListIter_at_end(iter)) {
+          tmp = Object_accept(ListIter_get_val(iter));
+          tmp2 = Object_accept(Lisp_eval_sexp2(tmp, env));
+          Object_bop_push(opargs2, tmp2);
+          Object_assign(&tmp, NULL);
+          Object_assign(&tmp2, NULL);
+          ListIter_next(iter);
+        }
+        ListIter_del(iter);
+        ret = Object_bop_call(opval, opargs2);
+      }
     }
   }
 _return:
