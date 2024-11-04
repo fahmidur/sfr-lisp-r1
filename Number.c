6992bc4178baa594899f06d6c006903948d5cfae
Syed Fahmidur Reza <s.f.reza@gmail.com>
2024-11-04 09:43:24 -0800

    Add sfr-lisp.c to tests against Racket
    - Add the C implementation of Lisp
      to be tested against Racket in the same way
      that the prototype implementation in Ruby
      is tested.
    - Fix print functions for Number class to use '%g'
      instead of '%f' format specifier.

--- Number.c
+++ Number.c
@@ -123,7 +123,7 @@ void Number_print_bare(Number* self) {
   if( self->rep - (long long)self->rep == 0.0) {
     printf("%lld", (long long)self->rep);
   } else {
-    printf("%f", self->rep);
+    printf("%g", self->rep);
   }
   Util_vt_set(VT_RESET);
 }
@@ -132,7 +132,7 @@ void Number_print(Number* self) {
   /*printf("Number(%f)", self->rep);*/
   printf("Number(");
   Util_vt_set(VT_COLOR_YELLOW_FG);
-  printf("%f", self->rep);
+  printf("%g", self->rep);
   Util_vt_set(VT_RESET);
   printf(")");
 }
--- test/tc_racket.rb
+++ test/tc_racket.rb
@@ -11,9 +11,16 @@ class TcRacket < Test::Unit::TestCase
       puts fpath
       racket_out = Util.run("racket #{fpath} | ./normalize-output.rb")
       assert_equal(0, racket_out.exitstatus)
+
       lisprb_out = Util.run("./sfr-lisp.rb #{fpath} | ./normalize-output.rb")
-      assert_equal(0, lisprb_out.exitstatus, "FAILURE in #{fpath}")
-      assert_equal(racket_out.stdout, lisprb_out.stdout, "FAILURE in #{fpath}")
+      assert_equal(0, lisprb_out.exitstatus, "sfr-lisp.rb | ecode FAILURE in #{fpath}")
+      assert_equal(racket_out.stdout, lisprb_out.stdout, "sfr-lisp.rb | stdout FAILURE in #{fpath}")
+
+      lispcc_out = Util.run("NO_COLOR=1 ./build/sfr-lisp #{fpath} | ./normalize-output.rb")
+      assert_equal(0, lispcc_out.exitstatus, "sfr-lisp.c | ecode FAILURE in #{fpath}")
+      assert_equal(racket_out.stdout, lispcc_out.stdout, "sfr-lisp.c | stdout FAILURE in #{fpath}")
+
+
     end
   end
 end
