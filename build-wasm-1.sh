1f69dbda3885fa6c23ac3874193b81cad50c0611
Syed Fahmidur Reza <s.f.reza@gmail.com>
2024-12-22 19:31:54 -0800

    wasm build. wip

---
+++ build-wasm-1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+/usr/lib/llvm-18/bin/clang \
+  -O3 \
+  -target wasm32-wasi \
+  --sysroot=/home/syed/xrepos/wasi-libc/sysroot \
+  -lc \
+  -o ./build/sfr-lisp.wasm \
+  sfr-lisp.c \
+  sub/linenoise/linenoise.c \
+  build/Util.o \
+  build/Object.o \
+  build/Symbol.o \
+  build/String.o \
+  build/Number.o \
+  build/List.o \
+  build/Hash.o \
+  build/Environment.o \
+  build/Function.o \
+  build/Runtime.o \
+  build/Lisp.o \
+  build/Error.o
