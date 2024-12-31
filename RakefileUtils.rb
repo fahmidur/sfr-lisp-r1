6195255ee8c2aa041a4815d580dd1e0403d7e7cb
Syed Fahmidur Reza <s.f.reza@gmail.com>
2024-12-31 01:00:58 -0800

    wip

--- RakefileUtils.rb
+++ RakefileUtils.rb
@@ -138,6 +138,7 @@ def compile(type, ofile, sources)
   elsif type == :object 
     com += " -c "
   elsif type == :wasm_program
+    com += " -D WASM"
     com += " -O3"
     com += " -target #{$conf['wasm_target']}"
     com += " --sysroot=#{$conf['wasm_sysroot']}"
--- Runtime.c
+++ Runtime.c
@@ -7,6 +7,7 @@ void Runtime_init() {
   ErrorSystem_init();
   Symbol_system_init();
   Object_system_init();
+  StringIO_init();
   dbg_printf("--- } Runtime_init() } ---\n");
   dbg_printf("\n\n");
 }
@@ -14,6 +15,7 @@ void Runtime_init() {
 void Runtime_done() {
   dbg_printf("\n\n");
   dbg_printf("--- { Runtime_done() { ---\n");
+  StringIO_done();
   Object_system_done();
   Symbol_system_done();
   ErrorSystem_done();
