1bba919a241bcfa61842aff8e9eb45afc4c1ceb6
Syed Fahmidur Reza <s.f.reza@gmail.com>
2024-12-30 22:04:30 -0800

    wip

--- sfr-lisp-wasm.c
+++ sfr-lisp-wasm.c
@@ -171,7 +171,11 @@ int main(int argc, char** argv) {
   int ret = 0;
   Runtime_init();
   Lisp_init();
-  printf("--- argc = %d\n", argc);
+  printf("argc = %d\n", argc);
+  int i;
+  for(i = 0; i < argc; i++) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
   if(argc == 2) {
     if(strcmp_onedash(argv[1], "-help", "-h") == 0) {
       print_usage(argv[0]);
