5a479b9e564b9eb04aebd6461ad73c75375128a1
Syed Fahmidur Reza <s.f.reza@gmail.com>
2024-12-31 12:32:31 -0800

    wip

--- wasm_webdriver/server.js
+++ wasm_webdriver/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(function(req, res, next) {
+  res.set('Cross-Origin-Opener-Policy', 'same-origin');
+  res.set('Cross-Origin-Resource-Policy', 'same-origin');
+  res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
+
 app.use(express.static('.'));
 
 app.listen(port);
