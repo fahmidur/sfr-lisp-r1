031ffc064a043dd4bd46d5c0d039dfd1a4a6fd6b
Syed Fahmidur Reza <s.f.reza@gmail.com>
2025-02-12 19:34:19 -0800

    wasm tests using wasmtime
    - Add wasm tests in tc_racket which
      tests implementation against Racket output.

--- test/tc_racket.rb
+++ test/tc_racket.rb
@@ -20,6 +20,10 @@ class TcRacket < Test::Unit::TestCase
       assert_equal(0, lispcc_out.exitstatus, "sfr-lisp.c | ecode FAILURE in #{fpath}")
       assert_equal(racket_out.stdout, lispcc_out.stdout, "sfr-lisp.c | stdout FAILURE in #{fpath}")
 
+      lispwasm_out = Util.run("wasmtime --dir . --env NO_COLOR=1 ./build/sfr-lisp-wasm.wasm #{fpath} | ./normalize-output.rb")
+      assert_equal(0, lispwasm_out.exitstatus, "sfr-lisp-wasm.c | ecode FAILURE in #{fpath}")
+      assert_equal(racket_out.stdout, lispwasm_out.stdout, "sfr-lisp-wasm.c | stdout FAILURE in #{fpath}")
+
 
     end
   end
