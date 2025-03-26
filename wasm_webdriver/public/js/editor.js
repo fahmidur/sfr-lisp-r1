79d323110021bcb61a83b0218bf8218c02b9ec33
Syed Fahmidur Reza <s.f.reza@gmail.com>
2025-03-26 01:44:11 -0700

    bring in codemirror 5

--- wasm_webdriver/public/js/editor.js
+++ wasm_webdriver/public/js/editor.js
@@ -1,6 +1,7 @@
 function Editor(el) {
   var self = this;
   self.els = {};
+  self.cm = null;
   self.el = self.elmake(el);
   console.log('Editor. self=', self);
 }
@@ -14,5 +15,11 @@ Editor.prototype.elmake = function(el) {
   var cmholder = $('<div></div>').addClass('cmholder');
   el.append(cmholder);
   self.els.cmholder = cmholder;
+  self.cm = new CodeMirror(cmholder.get(0), {
+    mode: 'scheme',
+    lineNumbers: true,
+    viewportMargin: +Infinity,
+    theme: 'nord'
+  });
   return el;
-}
+};
