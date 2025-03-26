7c03308a709e61a5516dfd51e97f1810099fca74
Syed Fahmidur Reza <s.f.reza@gmail.com>
2025-03-26 02:59:45 -0700

    wip

--- wasm_webdriver/public/js/worker2.js
+++ wasm_webdriver/public/js/worker2.js
@@ -320,7 +320,7 @@ onmessage = function(ev) {
         // make sure we push null character into the array
         stdin_arr.push(10); // LF character
         console.log('stdin_arr=', stdin_arr);
-        for(i = 0; i < stdin_arr.length; i++) {
+        for(let i = 0; i < stdin_arr.length; i++) {
           stdin_view[i] = stdin_arr[i];
         }
         stdin_size[0] = stdin_arr.length;
