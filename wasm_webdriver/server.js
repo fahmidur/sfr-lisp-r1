00e180ac13f5b72bd9ae45ccff2c8f46bbd7ca12
Syed Fahmidur Reza <s.f.reza@gmail.com>
2025-03-29 00:11:35 -0700

    wasm. add acme path for server

--- wasm_webdriver/server.js
+++ wasm_webdriver/server.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const redis = require('redis');
 const app = express();
 const port = 3000;
+
+var redis_client = redis.createClient();
+
+app.get('/.well-known/acme-challenge/:token', function(req, res) {
+  var log_prefix = 'acme.';
+  var rkey = 'ACME:'+req.params.token;
+  console.log(log_prefix, 'redis.get('+rkey+') = ... ');
+  redis_client.get(rkey, function(err, token) {
+    if(err) {
+      res.end('ERROR');
+      return;
+    }
+    console.log(log_prefix, 'redis.get('+rkey+') =', token);
+    res.end(token||'NULL'); 
+  });
+});
 
 app.use(function(req, res, next) {
   res.set('Cross-Origin-Opener-Policy', 'same-origin');
